4122a6208ba0efc5a681492284e0073d11109a40
Ben Smith <rios7467@gmail.com>
2017-04-24 17:26:22 -0600

    Added the meat to the sandwich. (Added some text and the basic structure of the page.)

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html lang="eng">
+	<head>
+		<title>Welcome to my Portfolio</title>
+		<meta charset="utf-8">
+
+		<!-- Latest compiled and minified Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+				integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+		<!-- MY Custom CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+	</head>
+	<body>
+		<div class="container">
+		<h1 class="col-sm-12">Welcome!</h1>
+		<h2 class="col-sm-12">My name is Ben Smith and this is my portfolio.</h2>
+		<img class="col-sm-12" id="top-image" src="">
+		<p class="col-sm-12" id="intro">I am a junior fullstack developer graduated from the CNM STEMulus Deep Dive Coding Bootcamp where I learned... well how to do this! If it weren't for them you wouldn't be reading this right now and I'd still be painting houses out in the sun. I have wanted to do stuff like this since I was a kid. I even took classes in middle school where I learned how to write basic commands in chipmunk basic. I was so excited to be able to make something using code that for an in class assignment that was only supposed to be over a couple days I actually spent tons of freetime on the weekend and after class just to draw a picture using coordinates. From there as I went through high school I took 4 years of computer graphics where I went over stuff from photoshop to some really simple coding stuff that was hyper limited though so I was a little disappointed until we started working with 3D wireframe models. It was amazing to see and learn how that stuff works. From making something as simple as a screw to a full character model and making it animate based on distance over time. Now that I actually know how to code some stuff a few friends and I are going to try to work at making some small scale video games by learning some new coding languages. Eventually I want to get into full scale game creation and development.</p>
+		<h3 class="col-sm-12">Aside from that wall of text I just threw at you I also have some work that I've done on a few sites.</h3>
+		<h4 class="col-sm-4"><a href="https://www.github.com/bsmith1188">Here is a link to my github.</a></h4>
+		<h4 class="col-sm-4"><a href="https://www.abquery.com">This is a link to the group project that we worked on.</a></h4>
+		<h4 class="col-sm-4"><a href="https://www.ballooncocoons.com">This is a site that I built on the side for my mom (still a work in progress)</a></h4>
+		</div>
+
+	</body>
+</html>
+
